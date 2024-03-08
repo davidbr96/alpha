5247f8052be795d3f08cb587d6e065eaf1e31211
davidbr96 <barronromandavid@gmail.com>
2024-03-08 09:50:06 -0700

    Alpha en angular

---
+++ src/app/alpha/alpha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alpha',
+  standalone: true,
+  imports: [],
+  templateUrl: './alpha.component.html',
+  styleUrl: './alpha.component.css'
+})
+export class AlphaComponent {
+
+}
